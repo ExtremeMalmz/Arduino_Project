dc1bb9d5089d894afd2294d9b62ac1c36230ce4d
Mr E <98057564+ExtremeMalmz@users.noreply.github.com>
2022-10-19 11:03:58 +0200

    Add files via upload

--- LightInTheDark.ino
+++ LightInTheDark.ino
@@ -1,7 +1,7 @@
 //manually setting the lamp to 13
 const int led0 = 13;
 //manually set weight to an integer
-const int weight = 0;
+const int weight = 1000;
 
 void setup() {
   // initialize digital pin 13 as an output.
@@ -14,7 +14,7 @@ void loop() {
   //if weight is greater than 1000 we send power into the device
   if(weight >= 1000){
     digitalWrite(led0,HIGH); //sets the power to HIGH as to create voltage
-    delay(1000); //wait a while
+    delay(10000); //wait a while
     digitalWrite(led0,LOW); //sets power to low to create a flashing effect
     delay(1000); //wait a while
   }
